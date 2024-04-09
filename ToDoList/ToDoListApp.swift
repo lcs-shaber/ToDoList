bb6fd8279b6604883ebaa05132a64d4374722ae7
Sophie Haber <SHaber@lcs.on.ca>
2024-04-09 14:57:48 -0400

    Added a model container to the app entry point.

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -5,6 +5,7 @@
 //  Created by Sophie Haber on 07.04.24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -12,6 +13,7 @@ struct ToDoListApp: App {
     var body: some Scene {
         WindowGroup {
             LandingView()
+                .modelContainer(for: TodoItem.self)
         }
     }
 }
